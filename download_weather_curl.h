eb64f717e14a428dd636efc14eb0be2fda6b46a8
Den-Git-CPP <madim-neodart@rambler.ru>
2022-03-06 11:20:58 +0300

    Скорректирована ссылка в dowload_weather_curl.h для скачивания только крайней сводки

--- download_weather_curl.h
+++ download_weather_curl.h
@@ -6,16 +6,28 @@
 #include <iostream>
 #include <fstream>
 
+#include <ratio>
+#include <chrono>
+
 static size_t write_data(void* ptr, size_t size, size_t nmemb, void* stream) {
     size_t written = fwrite(ptr, size, nmemb, static_cast<FILE*>(stream));
     return written;
 } /* write_data */
 
 void DownloadFile(std::string &ICAO_airport_name) {
-    std::string http_link_airport_weather ="https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=csv&stationString="+ICAO_airport_name+"%20&hoursBeforeNow=1";
+   std::chrono::duration<int, std::ratio<14400> > delta(1);
+    std::chrono::system_clock::time_point end = std::chrono::system_clock::now();
+    std::chrono::system_clock::time_point start = end - delta;
+     
+    std::string http_link_airport_weather ="https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=tafs&requestType=retrieve&format=csv&startTime="
+    +std::to_string(std::chrono::system_clock::to_time_t(start))
+    +"&endTime="
+    + std::to_string(std::chrono::system_clock::to_time_t(end))
+    +"&timeType=issue&stationString="+ ICAO_airport_name;
+
     std::string local_pth_airport_weather = ICAO_airport_name+".txt";
     // Download the file using curl library into DownloadCURL folder
-     
+         
     if(CURL* curl = curl_easy_init()) 
     {   CURLcode res;
         FILE* fp = fopen(local_pth_airport_weather.c_str(), "wb");
